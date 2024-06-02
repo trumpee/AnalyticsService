4bf125e28b92119fca318e75cde2d8b9ce649add
Bardin08 <vbardin810@gmail.com>
2024-06-02 14:54:46 +0300

    Add mongo connector

---
+++ AnalyticsService/Infrastructure/Persistence/Mongo/Abstractions/IMongoRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace AnalyticsService.Infrastructure.Persistence.Mongo.Abstractions;
+
+public interface IMongoRepository<in TEntity>
+{
+    Task InsertOne(TEntity document);
+    Task InsertMany(IEnumerable<TEntity> documents);
+}
---
+++ AnalyticsService/Infrastructure/Persistence/Mongo/Repositories/MongoDbRepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using AnalyticsService.Infrastructure.Persistence.Mongo.Abstractions;
+using AnalyticsService.Infrastructure.Persistence.Mongo.Configurations;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace AnalyticsService.Infrastructure.Persistence.Mongo.Repositories;
+
+internal abstract class MongoDbRepositoryBase<T> : IMongoRepository<T>
+    where T : Entities.MongoBaseEntity
+{
+    private readonly IMongoCollection<T> _collection;
+
+    protected MongoDbRepositoryBase(
+        IOptions<MongoDbOptions> settings)
+    {
+        var settingValue = settings.Value;
+        var database = new MongoClient(settingValue.ConnectionString).GetDatabase(settingValue.DatabaseName);
+        _collection = database.GetCollection<T>(typeof(T).Name);
+    }
+
+    public Task InsertOne(T document)
+        => _collection.InsertOneAsync(document);
+
+    public Task InsertMany(IEnumerable<T> documents)
+        => _collection.InsertManyAsync(documents);
+}
